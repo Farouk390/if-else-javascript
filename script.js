44dee0222a4ff6f2808f722cf05f3c018cb53baa
Farouk390 <faroukmohamadalbitar@gmail.com>
2026-06-08 11:31:42 +0200

    l

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var button = document.querySelector("button");
 var box = document.getElementById("ChangeMe");
 
 function ChangeColor() {
-if (box.style.background == "rgb(255, 255, 255)"){
+if (box.style.background == "rgb(0, 0, 255)"){
 
   box.style.background = "rgb(13, 27, 42)";
 
  }else {
 
-box.style.background = "rgb(121, 179, 255)";
+box.style.background = "rgb(0, 0, 255)";
  }
 }
